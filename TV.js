e10c5d01ce3166b92b864eca02dcbd8f245bc30b
Shreyansh222 <92247523+Shreyansh222@users.noreply.github.com>
2022-09-21 18:58:57 +0530

    Add files via upload

---
+++ TV.js
@@ -0,0 +1,47 @@
+Status = "";
+TV_Image = "";
+
+function preload()
+{
+TV_Image = loadImage("TV_Image.jpg");
+}
+
+function setup(){
+canvas = createCanvas(640,420);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+console.log("Model Loaded!");
+Status = true;
+objectDetector.detect(TV_Image,gotResults);
+}
+
+function gotResults(error,results)
+{
+if(error)
+{
+console.error(error);
+}
+console.log(results);
+}
+
+function draw()
+{
+image(TV_Image,0,0,640,350);
+if(Status != ""){for(i = 0; i < objects.length; i++)
+{
+document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+fill("#00FFFF");
+percent = floor(objects[i].confidence * 100);
+text(objects[i].label + " " + percent + "%",objects[i].x - 180, objects[i].y - 200);
+noFill();
+stroke("#00FFFF");
+rect(objects[i].x - 180, objects[i].y - 200, objects[i].width - 2693, objects[i].height - 1750);
+}
+}
+}
